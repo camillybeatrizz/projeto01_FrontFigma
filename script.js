68fce6b792103c6fbc49ec27f568fb32e17df8cc
Camilly Beatriz <camillybloliveira@gmail.com>
2025-05-20 21:41:51 -0300

    Projeto Inicial
    Sem atualizações ainda.

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleSobre() {
+    const sobreText = document.getElementById('sobreText');
+    sobreText.classList.toggle('open');
+}
+
+function toggleHabilidades() {
+    const habilidadesText = document.getElementById('habilidadesText');
+    habilidadesText.classList.toggle('open');
+}
+
+function toggleProjetos() {
+    const projetosText = document.getElementById('projetosText');
+    projetosText.classList.toggle('open');
+}
+
+function toggleExperiencias() {
+    const experienciasText = document.getElementById('experienciasText');
+    experienciasText.classList.toggle('open');
+}
+
+function toggleContato() {
+    const contatoText = document.getElementById('contatoText');
+    contatoText.classList.toggle('open');
+}
