d0a81be870d525c40f844269ba69b07de183e13e
Camilly Beatriz <camillybloliveira@gmail.com>
2025-05-21 22:13:18 -0300

    Atualizações do Projeto do Portfólio com Figma - 01

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleSobre() {
+    const sobreText = document.getElementById('sobreText');
+    sobreText.classList.toggle('open');
+}
+
+function toggleHabilidades() {
+    const habilidadesText = document.getElementById('habilidadesText');
+    habilidadesText.classList.toggle('open');
+}
+
+function toggleProjetos() {
+    const projetosText = document.getElementById('projetosText');
+    projetosText.classList.toggle('open');
+}
+
+function toggleExperiencias() {
+    const experienciasText = document.getElementById('experienciasText');
+    experienciasText.classList.toggle('open');
+}
+
+function toggleContato() {
+    const contatoText = document.getElementById('contatoText');
+    contatoText.classList.toggle('open');
+}
+
+function toggleCertificados() {
+    const certificadosText = document.getElementById('certificadosText');
+    certificadosText.classList.toggle('open');
+}
